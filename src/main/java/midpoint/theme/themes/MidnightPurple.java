8afad14c4eb394f331a33d005b39080b2807b035
letov7482-ui <letov7482@gmail.com>
2026-07-25 17:21:31 +0300

    Create MidnightPurple.java

---
+++ src/main/java/midpoint/theme/themes/MidnightPurple.java
@@ -0,0 +1,31 @@
+package midpoint.theme.themes;
+
+
+import midpoint.theme.Theme;
+import midpoint.render.util.ColorUtil;
+
+
+public class MidnightPurple extends Theme {
+
+
+    public MidnightPurple() {
+
+        super(
+                "Midnight Purple",
+
+                ColorUtil.rgb(
+                        139,
+                        92,
+                        246
+                ),
+
+                ColorUtil.rgb(
+                        45,
+                        35,
+                        70
+                )
+        );
+
+    }
+
+}
