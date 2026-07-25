0135ae8ed5d078106d9fc02b58230e32eda32c01
letov7482-ui <letov7482@gmail.com>
2026-07-25 22:28:51 +0300

    Update Event.java

--- src/main/java/midpoint/event/Event.java
+++ src/main/java/midpoint/event/Event.java
@@ -4,11 +4,18 @@ public abstract class Event {
 
     private boolean cancelled;
 
+
     public boolean isCancelled() {
+
         return cancelled;
+
     }
 
-    public void cancel() {
-        this.cancelled = true;
+
+    public void setCancelled(boolean cancelled) {
+
+        this.cancelled = cancelled;
+
     }
+
 }
