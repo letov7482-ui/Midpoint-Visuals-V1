168a7c5dc5993fd755bad16de2153a27ebc9361f
letov7482-ui <letov7482@gmail.com>
2026-07-25 17:42:43 +0300

    Create InGameHudMixin.java

---
+++ src/main/java/midpoint/mixin/InGameHudMixin.java
@@ -0,0 +1,32 @@
+package midpoint.mixin;
+
+import midpoint.hud.Huds;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+
+
+    @Inject(
+            method = "render",
+            at = @At("TAIL")
+    )
+    private void renderMidpointHud(
+            DrawContext context,
+            float tickDelta,
+            CallbackInfo ci
+    ) {
+
+        Huds.MANAGER.renderAll();
+
+    }
+
+}
