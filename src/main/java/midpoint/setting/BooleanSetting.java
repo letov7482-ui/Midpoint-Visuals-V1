85f94a3d7670cd9b360981c7dd65d7c33e3517f1
letov7482-ui <letov7482@gmail.com>
2026-07-25 20:20:35 +0300

    Update BooleanSetting.java

--- src/main/java/midpoint/setting/BooleanSetting.java
+++ src/main/java/midpoint/setting/BooleanSetting.java
@@ -2,13 +2,17 @@ package midpoint.setting;
 
 public class BooleanSetting extends Setting<Boolean> {
 
-
     public BooleanSetting(
             String name,
             boolean value
     ) {
-
         super(name, value);
+    }
+
+
+    public boolean get() {
+
+        return getValue();
 
     }
 
