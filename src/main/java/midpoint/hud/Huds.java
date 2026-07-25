7007ad80e73dafe4773667e6d6b2e1e023b6c28b
letov7482-ui <letov7482@gmail.com>
2026-07-25 17:37:09 +0300

    Create Huds.java

---
+++ src/main/java/midpoint/hud/Huds.java
@@ -0,0 +1,20 @@
+package midpoint.hud;
+
+
+public class Huds {
+
+
+    public static final HudManager MANAGER =
+            new HudManager();
+
+
+
+    public static void initialize() {
+
+
+        // Здесь будут HUD элементы
+
+
+    }
+
+}
