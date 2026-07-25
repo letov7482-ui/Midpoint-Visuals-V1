b98ffe56abf253bc007ccab0725e32859ad180d6
letov7482-ui <letov7482@gmail.com>
2026-07-25 17:22:52 +0300

    Update Managers.java

--- src/main/java/midpoint/core/Managers.java
+++ src/main/java/midpoint/core/Managers.java
@@ -1,14 +1,27 @@
 package midpoint.core;
 
+
 import midpoint.module.Modules;
+import midpoint.theme.ThemeManager;
+
 
 public class Managers {
 
 
+    public static final ThemeManager THEME =
+            new ThemeManager();
+
+
+
     public static void initialize() {
+
+
+        THEME.initialize();
 
         Modules.initialize();
 
+
     }
+
 
 }
