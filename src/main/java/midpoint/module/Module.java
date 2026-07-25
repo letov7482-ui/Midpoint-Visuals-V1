e370e57704e69f438c63a2a86772c129e72978ab
letov7482-ui <letov7482@gmail.com>
2026-07-25 17:08:18 +0300

    Update Module.java

--- src/main/java/midpoint/module/Module.java
+++ src/main/java/midpoint/module/Module.java
@@ -1,5 +1,10 @@
 package midpoint.module;
 
+import midpoint.module.settings.Setting;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Module {
 
     private final String name;
@@ -7,10 +12,14 @@ public abstract class Module {
 
     private boolean enabled;
 
+    private final List<Setting<?>> settings = new ArrayList<>();
+
 
     public Module(String name, Category category) {
+
         this.name = name;
         this.category = category;
+
     }
 
 
@@ -23,29 +32,58 @@ public abstract class Module {
 
     public void setEnabled(boolean enabled) {
 
+        if (this.enabled == enabled) {
+            return;
+        }
+
         this.enabled = enabled;
 
+
         if (enabled) {
+
             onEnable();
+
         } else {
+
             onDisable();
+
         }
 
     }
 
 
     public boolean isEnabled() {
+
         return enabled;
+
     }
 
 
     public String getName() {
+
         return name;
+
     }
 
 
     public Category getCategory() {
+
         return category;
+
+    }
+
+
+    public void addSetting(Setting<?> setting) {
+
+        settings.add(setting);
+
+    }
+
+
+    public List<Setting<?>> getSettings() {
+
+        return settings;
+
     }
 
 
