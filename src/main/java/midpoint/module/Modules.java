24911dd7a67a760aeed01eb45580dcd5b7030d86
letov7482-ui <letov7482@gmail.com>
2026-07-25 17:00:53 +0300

    Create Modules.java

---
+++ src/main/java/midpoint/module/Modules.java
@@ -0,0 +1,17 @@
+package midpoint.module;
+
+public class Modules {
+
+    public static final ModuleManager MANAGER =
+            new ModuleManager();
+
+
+    public static void initialize() {
+
+        /*
+         * Здесь будут регистрироваться модули
+         */
+
+    }
+
+}
