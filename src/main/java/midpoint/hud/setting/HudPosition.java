ddb109327cc63ea224e311e14d567ad0b4b7a4ff
letov7482-ui <letov7482@gmail.com>
2026-07-26 12:21:18 +0300

    Create HudPosition.java

---
+++ src/main/java/midpoint/hud/setting/HudPosition.java
@@ -0,0 +1,40 @@
+package midpoint.hud.setting;
+
+
+public class HudPosition {
+
+    private float x;
+    private float y;
+
+
+    public HudPosition(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public float getX() {
+        return x;
+    }
+
+
+    public float getY() {
+        return y;
+    }
+
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+
+    public void setPosition(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
