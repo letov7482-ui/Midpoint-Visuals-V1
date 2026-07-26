dbd28397d8bf84e3b796ce6520acd6a242b2e0cb
letov7482-ui <letov7482@gmail.com>
2026-07-26 12:24:06 +0300

    Update HudEditor.java

--- src/main/java/midpoint/hud/HudEditor.java
+++ src/main/java/midpoint/hud/HudEditor.java
@@ -2,20 +2,24 @@ package midpoint.hud;
 
 import midpoint.hud.element.HudElement;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
-
 public class HudEditor {
 
     private static boolean editing = false;
 
     private static HudElement dragging;
+
     private static float offsetX;
     private static float offsetY;
 
 
-    public static void setEditing(boolean value) {
-        editing = value;
+    public static void enable() {
+        editing = true;
+    }
+
+
+    public static void disable() {
+        editing = false;
+        dragging = null;
     }
 
 
@@ -32,12 +36,16 @@ public class HudEditor {
 
         for (HudElement element : Huds.MANAGER.getElements()) {
 
-            if (isInside(element, mouseX, mouseY)) {
+            if (mouseX >= element.getX()
+                    && mouseX <= element.getX() + 150
+                    && mouseY >= element.getY()
+                    && mouseY <= element.getY() + 20) {
+
 
                 dragging = element;
 
-                offsetX = (float)mouseX - element.getX();
-                offsetY = (float)mouseY - element.getY();
+                offsetX = (float) mouseX - element.getX();
+                offsetY = (float) mouseY - element.getY();
 
                 break;
             }
@@ -52,8 +60,8 @@ public class HudEditor {
 
 
         dragging.setPosition(
-                (float)mouseX - offsetX,
-                (float)mouseY - offsetY
+                (float) mouseX - offsetX,
+                (float) mouseY - offsetY
         );
     }
 
@@ -61,20 +69,6 @@ public class HudEditor {
     public static void mouseReleased() {
 
         dragging = null;
-
-    }
-
-
-    private static boolean isInside(
-            HudElement element,
-            double mouseX,
-            double mouseY
-    ) {
-
-        return mouseX >= element.getX()
-                && mouseX <= element.getX()+120
-                && mouseY >= element.getY()
-                && mouseY <= element.getY()+20;
 
     }
 }
