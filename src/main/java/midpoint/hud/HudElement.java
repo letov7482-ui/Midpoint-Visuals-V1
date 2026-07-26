9e3c533c7dcc807c1054a1dfeab571b91d51f8e2
letov7482-ui <letov7482@gmail.com>
2026-07-26 14:04:13 +0300

    Update HudElement.java

--- src/main/java/midpoint/hud/HudElement.java
+++ src/main/java/midpoint/hud/HudElement.java
@@ -1,79 +1,40 @@
-package midpoint.hud.element;
+package midpoint.hud;
 
 import midpoint.hud.setting.HudPosition;
-
 import net.minecraft.client.gui.DrawContext;
-
 
 public abstract class HudElement {
 
-    protected HudPosition position;
-
+    private final HudPosition position;
     private boolean enabled = true;
 
-
     public HudElement(float x, float y) {
-
         this.position = new HudPosition(x, y);
-
     }
-
 
     public abstract void render(DrawContext context);
 
-
-
     public float getX() {
-
         return position.getX();
-
     }
-
 
     public float getY() {
-
         return position.getY();
-
     }
-
-
 
     public void setPosition(float x, float y) {
-
         position.setPosition(x, y);
-
     }
-
-
-
-    public void loadPosition(float x, float y) {
-
-        position.setPosition(x, y);
-
-    }
-
-
 
     public HudPosition getPosition() {
-
         return position;
-
     }
-
-
 
     public boolean isEnabled() {
-
         return enabled;
-
     }
-
-
 
     public void setEnabled(boolean enabled) {
-
         this.enabled = enabled;
-
     }
-
 }
