cc86c512d8b9df4f1ee861f928f5b5ac27c09eb3
letov7482-ui <letov7482@gmail.com>
2026-07-26 14:39:28 +0300

    Update HudPosition.java

--- src/main/java/midpoint/hud/setting/HudPosition.java
+++ src/main/java/midpoint/hud/setting/HudPosition.java
@@ -3,38 +3,68 @@ package midpoint.hud.setting;
 
 public class HudPosition {
 
+
     private float x;
     private float y;
 
 
-    public HudPosition(float x, float y) {
+
+    public HudPosition(
+            float x,
+            float y
+    ) {
+
         this.x = x;
         this.y = y;
+
     }
+
 
 
     public float getX() {
+
         return x;
+
     }
+
 
 
     public float getY() {
+
         return y;
+
     }
 
 
-    public void setX(float x) {
+
+    public void setX(
+            float x
+    ) {
+
         this.x = x;
+
     }
 
 
-    public void setY(float y) {
+
+    public void setY(
+            float y
+    ) {
+
         this.y = y;
+
     }
 
 
-    public void setPosition(float x, float y) {
+
+    public void set(
+            float x,
+            float y
+    ) {
+
         this.x = x;
         this.y = y;
+
     }
+
 }
