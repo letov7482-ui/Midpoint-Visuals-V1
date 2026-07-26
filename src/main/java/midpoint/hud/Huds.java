de41d904c7544d4d700957211d79e7b89931a645
letov7482-ui <letov7482@gmail.com>
2026-07-26 14:08:37 +0300

    Update Huds.java

--- src/main/java/midpoint/hud/Huds.java
+++ src/main/java/midpoint/hud/Huds.java
@@ -2,22 +2,13 @@ package midpoint.hud;
 
 import midpoint.hud.elements.Watermark;
 
-
 public class Huds {
 
-
-    public static final HudManager MANAGER =
-            new HudManager();
-
-
+    public static final HudManager MANAGER = new HudManager();
 
     public static void initialize() {
 
-
-        MANAGER.register(
-                new Watermark()
-        );
-
+        MANAGER.register(new Watermark());
 
     }
 
